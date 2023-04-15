72861b7ad95108c8e47aa46e8fc6ffd893aabd61
jlmansilla <juanmancillaster@gmail.com>
2023-04-15 12:59:19 -0400

    agrega relaciones a los modelos

--- app/models/kindergarten.rb
+++ app/models/kindergarten.rb
@@ -2,4 +2,6 @@ class Kindergarten < ApplicationRecord
   belongs_to :city
   belongs_to :user
   has_one_attached :photo
+  has_many :kindergarten_pets
+  has_many :pets, through: :kindergarten_pets
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,4 @@
 class Pet < ApplicationRecord
+  has_many :kindergarten_pets
+  has_many :kindergartens, through: :kindergarten_pets
 end
