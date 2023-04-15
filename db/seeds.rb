9cb9175dd061df93a98d10e7f9dd99e637299fd7
jlmansilla <juanmancillaster@gmail.com>
2023-04-15 15:05:34 -0400

    genera los datos con faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+10.times do
+  User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: 'password'
+  )
+end
+  
+10.times do
+  City.create!(
+    name: Faker::Address.city
+  )
+end
+  
+10.times do
+  Pet.create!(
+    name: Faker::Creature::Animal.name
+  )
+end
+  
+cities = City.all
+users = User.all
+  
+20.times do
+  kindergarten = Kindergarten.create!(
+    name: Faker::Company.name,
+    description: Faker::Lorem.paragraph,
+    photo: Faker::LoremPixel.image,
+    city: cities.sample,
+    price: Faker::Commerce.price(range: 50..500),
+    phone: Faker::PhoneNumber.phone_number,
+    user: users.sample
+  )
+end
+  
+pets = Pet.all
+kindergartens = Kindergarten.all
+
+50.times do
+  kindergarten_pet = KindergartenPet.create!(
+    pet: pets.sample,
+    kindergarten: kindergartens.sample,
+    compatible: Faker::Boolean.boolean(true_ratio: 0.7)
+  )
+end
+  
